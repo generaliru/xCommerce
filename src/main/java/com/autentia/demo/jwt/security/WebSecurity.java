eed13b037b4ee99fe368fde76a1a01923358ed5c
Yann <metalyanni@gmail.com>
2021-08-04 18:05:06 -0500

    Configuración del CORS y añadiendo el token en los headers

--- src/main/java/com/autentia/demo/jwt/security/WebSecurity.java
+++ src/main/java/com/autentia/demo/jwt/security/WebSecurity.java
@@ -1,5 +1,6 @@
 package com.autentia.demo.jwt.security;
 
+import static com.autentia.demo.jwt.security.Constants.HEADER_AUTHORIZACION_KEY;
 import static com.autentia.demo.jwt.security.Constants.LOGIN_URL;
 
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -58,10 +61,19 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 
+	/*Configuración del CORS*/
 	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-		return source;
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				/*addMapping(/**) Le decimos que son todas las rutas
+				 * allowedOrigins("*") permite las peticiones por todos lados
+				 * */
+				registry.addMapping("/**").allowedOrigins("*")
+				.exposedHeaders(HEADER_AUTHORIZACION_KEY);
+				//Aqui configuramos que headers sean visibles:
+			}
+		};
 	}
 }
