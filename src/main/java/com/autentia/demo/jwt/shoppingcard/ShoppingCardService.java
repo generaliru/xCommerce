3425c2c0f4e7e47f895d280acb360f0eaa56a61e
Yann <metalyanni@gmail.com>
2021-08-09 11:54:12 -0500

    Agregando campos en usuario

--- src/main/java/com/autentia/demo/jwt/shoppingcard/ShoppingCardService.java
+++ src/main/java/com/autentia/demo/jwt/shoppingcard/ShoppingCardService.java
@@ -3,13 +3,20 @@ package com.autentia.demo.jwt.shoppingcard;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import com.autentia.demo.jwt.usuario.Usuario;
+import com.autentia.demo.jwt.usuario.UsuarioRepository;
 
 @Service
 public class ShoppingCardService {
 	
 	@Autowired
-ShoppingCardRepository shoppingcardRepository;
+	ShoppingCardRepository shoppingcardRepository;
+	
+	@Autowired
+	UsuarioRepository usuarioRepository;
 	
 	public ArrayList<ShoppingCardModel> getCards(){
 		return (ArrayList<ShoppingCardModel>) shoppingcardRepository.findAll();
@@ -25,8 +32,14 @@ ShoppingCardRepository shoppingcardRepository;
 		return card;
 	}
 
-
- 
+	/**
+	 * Obtains a ShoppingCart by the token of login of the user.
+	 * @return ShoppingCardModel return the cart asigment to user.
+	 * */
+	public ShoppingCardModel getCartUser() {
+		Usuario usuario = usuarioRepository.findByUsername((String) (SecurityContextHolder.getContext().getAuthentication().getPrincipal()));
+		return usuario.getShoppingCard();
+	}
 
 
 }
